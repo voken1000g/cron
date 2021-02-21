af2871521678b9d853d49d373d7b78481ee7b1bb
Lyman Snow <pmvoken@protonmail.com>
2021-02-21 18:14:29 +0000

    updated: From

--- debug.js
+++ debug.js
@@ -11,7 +11,7 @@ const getIpData = async () => {
 const main = async () => {
   const ipData = await getIpData()
 
-  const message = '*GitHub Actions - debug.js*\n\n`From ' + ipData.ip + ' ' + ipData.country + '`'
+  const message = '*GitHub Actions - debug.js*\n\n`From: ' + ipData.ip + ' (' + ipData.country + ')`'
 
   await bot.sendMessage(process.env.TG_DEV_CHAT_ID, message, {parse_mode : "MARKDOWN"})
 }
