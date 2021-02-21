4021360425d514a203477a338796f3dc9410cb16
Lyman Snow <pmvoken@protonmail.com>
2021-02-21 16:58:40 +0000

    rename: TG*

--- debug.js
+++ debug.js
@@ -1,9 +1,7 @@
 const fetch = require('node-fetch')
 const Telegram = require('node-telegram-bot-api')
 
-const TG_BOT_TOKEN = process.env.TG_BOT_TOKEN
-const TG_CHAT_ID = process.env.TG_CHAT_ID
-const bot = new Telegram(TG_BOT_TOKEN)
+const bot = new Telegram(process.env.TG_DEV_BOT_TOKEN)
 
 const getIpData = async () => {
   const resp = await fetch('https://api.myip.com/')
@@ -15,7 +13,7 @@ const main = async () => {
 
   const message = '*GitHub Actions*\n\n`From ' + ipData.ip + ' ' + ipData.country + '`'
 
-  await bot.sendMessage(TG_CHAT_ID, message, {parse_mode : "MARKDOWN"})
+  await bot.sendMessage(process.env.TG_DEV_CHAT_ID, message, {parse_mode : "MARKDOWN"})
 }
 
 main()
